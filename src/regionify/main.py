e538509e1cb7bb6920c4f88145957ff88af3599c
Nahum Timerman <ntimerman@gmail.com>
2017-01-05 10:46:27 +0200

    aws has aws packages, azure has azure

--- src/regionify/main.py
+++ src/regionify/main.py
@@ -3,20 +3,28 @@ from src.regionify.cloudshell_package import CloudshellPackage
 from distutils import dir_util
 import os
 
+AZURE_INVOICE = '../regionify/invoices/azure_app_invoice.xlsx'
+
+AWS_INVOICE = '../regionify/invoices/aws_app_invoice.xlsx'
+
+AZURE_SOURCE_CLOUDSHELL_PACKAGE = '../cloudshell_package/azure'
+
+AWS_SOURCE_CLOUDSHELL_PACKAGE = '../cloudshell_package/aws'
+
 AWS_DIRECTORY = '..\\..\\Packages\\Amazon'
 AZURE_DIRECTORY = '..\\..\\Packages\\Azure'
 
 
 def main():
-    aws_package = CloudshellPackage('../cloudshell_package/azure')
-    aws_invoice = AppInvoice('../regionify/invoices/azure_app_invoice.xlsx')
-    for params in aws_invoice.azure_cloud_provider_params():
+    aws_package = CloudshellPackage(AWS_SOURCE_CLOUDSHELL_PACKAGE)
+    aws_invoice = AppInvoice(AWS_INVOICE)
+    for params in aws_invoice.aws_cloud_provider_params():
         target_path = os.path.join(AWS_DIRECTORY, params.app_name)
         dir_util.mkpath(target_path)
-        aws_package.prepare_azure_package(params, target_path)
+        aws_package.prepare_aws_package(params, target_path)
 
-    azure_package = CloudshellPackage('../cloudshell_package/azure')
-    azure_invoice = AppInvoice('../regionify/invoices/azure_app_invoice.xlsx')
+    azure_package = CloudshellPackage(AZURE_SOURCE_CLOUDSHELL_PACKAGE)
+    azure_invoice = AppInvoice(AZURE_INVOICE)
     for params in azure_invoice.azure_cloud_provider_params():
         target_path = os.path.join(AZURE_DIRECTORY, params.app_name)
         dir_util.mkpath(target_path)
